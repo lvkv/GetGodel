2c72312cf85bde3eafd5556fec49e2ddc27dac1e
Lukas Velikov <lukas@rytas.io>
2020-05-06 16:26:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import random
 import enum
 import sys
-
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QLabel, QMessageBox
 
 
 class Direction(enum.Enum):
+    """
+    The value of each direction is the number of 90° clockwise board rotations needed to make swiping in <Direction>
+    equivalent to to swiping Left.
+    """
     LEFT = 0
     DOWN = 1
     RIGHT = 2
     UP = 3
 
 
-def rotate_list_right(board):
-    return [list(r) for r in zip(*board[::-1])]
+def rotate_list_right(x):
+    """Returns list x but rotated 90° clockwise. I didn't write this. To be honest, it's an abomination."""
+    return [list(r) for r in zip(*x[::-1])]
 
 
 class Board:
-    def __init__(self, h, w):
-        self.h = h
-        self.w = w
-        self.board = [[None] * w for i in range(h)]
+    """
+    A representation of the game's state (aka the board!).
+    Supports rectangular, not just square, boards.
+    """
+
+    def __init__(self, height, width):
+        self.board = [[None] * width for i in range(height)]  # None corresponds to an empty tile
 
     def insert_tile(self, val, ri, ci):
+        """Change the value of the tile located at coordinates (ri, ci) to val"""
         self.board[ri][ci] = val
 
     def insert_random_tile(self):
+        """Inserts a 2 (Church) or 4 (Turing) into a random empty tile. Returns False if no empty tiles exist."""
         rand_val = random.choice([2, 4])
         empty_tiles = []
         for ri, row in enumerate(self.board):
@@ -36,13 +45,17 @@ class Board:
         if len(empty_tiles) > 0:
             rand_row, rand_col = random.choice(empty_tiles)
             self.insert_tile(rand_val, rand_row, rand_col)
-            return rand_row, rand_col
-        return None
+            return True
+        return False
 
     def shift(self, direction):
-        rotations_needed = direction.value
-        board_rotated = self.board
-        for i in range(rotations_needed):
+        """
+        Update the state of the board to that after swiping in Direction direction.
+        Returns True if shifting changed the state of the board, False if it didn't.
+        """
+        rotations_needed = direction.value  # This problem can actually be solved in a general way
+        board_rotated = self.board.copy()
+        for i in range(rotations_needed):  # We just need to rotate the board based on the direction
             board_rotated = rotate_list_right(board_rotated)
         for row_index, row in enumerate(board_rotated):
             prev_val = None
@@ -62,49 +75,52 @@ class Board:
             for i in range(len_w_none - len_wo_none):
                 new_row.append(None)
             board_rotated[row_index] = new_row
-        for i in range(4 - rotations_needed):
+        for i in range(4 - rotations_needed):  # Un-rotate the board after we're done
             board_rotated = rotate_list_right(board_rotated)
+        something_changed = self.board != board_rotated
         self.board = board_rotated
-
-    def display(self):
-        for row in self.board:
-            for item in row:
-                if item is None:
-                    item = 0
-                print(f'{item}\t\t', end='')
-            print()
+        return something_changed
 
 
 class App(QWidget):
-    key_mappings = {87: Direction.UP, 83: Direction.DOWN, 65: Direction.LEFT, 68: Direction.RIGHT}
-    number_mappings = {
+    """Represents the GUI and drives the game."""
+
+    key_mappings = {  # Maps keyboard codes to directions
+        87: Direction.UP,  # 'W'
+        83: Direction.DOWN,  # 'A'
+        65: Direction.LEFT,  # 'S'
+        68: Direction.RIGHT  # 'D'
+    }
+
+    number_mappings = {  # Maps tile values to images of logicians and philosophers
         None: 'images/empty.jpg',
         2: 'images/church.jpg',
-        4: 'images/turing.jpg',
+        4: 'images/turing.jpg',  # Church-Turing hypothesis
         8: 'images/frege.jpg',
-        16: 'images/russell.jpg',
+        16: 'images/russell.jpg',  # Frege's theorem and Russell's paradox
         32: 'images/post.jpg',
-        64: 'images/quine.jpg',
+        64: 'images/quine.jpg',  # Our 'Post and Quine' puzzles
         128: 'images/curry.jpg',
-        256: 'images/hilbert.jpg',
+        256: 'images/hilbert.jpg',  # Obligatory Hilbert for Mei Rose
         512: 'images/tarski.jpg',
         1024: 'images/montague.jpg',
-        2048: 'images/goedel.jpg',
+        2048: 'images/goedel.jpg',  # The game is 'incomplete' until you get to Godel :)
     }
 
     def __init__(self):
         super().__init__()
-        board_side_len = 4
-        board = Board(board_side_len, board_side_len)
-        self.board = board
+        self.board = Board(4, 4)
         self.title = 'Get Godel'
         self.grid = None
-        self.waiting = False
+        self.window_text = None
         self.init_ui()
 
     def init_ui(self):
+        """Initializes the window with a layout and a grid"""
         self.setWindowTitle(self.title)
         window_layout = QVBoxLayout()
+        self.window_text = QLabel('Use the W, A, S, D keys to combine logicians. Get to Godel!')
+        window_layout.addWidget(self.window_text)
         grid_layout = QGridLayout()
         for row_index, row in enumerate(self.board.board):
             for col_index in range(len(row)):
@@ -115,21 +131,37 @@ class App(QWidget):
         self.grid = grid_layout
         self.setLayout(window_layout)
         self.show()
+        self.board.insert_random_tile()
+        self.update_board_gui()
+
+    def check_for_win(self):
+        for row in self.board.board:
+            for item in row:
+                if item == 2048:
+                    self.window_text.setText('You got to Godel!')
+                    App.key_mappings = {}  # Big brain way of stopping the game
+
+    def lose(self):
+        self.window_text.setText('Oh no, you lost! Restart the app to try again.')
 
     def keyPressEvent(self, event):
+        """Triggered when a key is pressed. Advances the game if the pressed key has a mapping"""
         key = event.key()
         if key in App.key_mappings:
             self.board.shift(App.key_mappings[key])
-            self.board.insert_random_tile()
+            tile_inserted = self.board.insert_random_tile()
             self.update_board_gui()
+            if not tile_inserted:  # If there's no more space to insert tiles, the player has lost
+                self.lose()
+            self.check_for_win()
 
     def update_board_gui(self):
+        """Updates the GUI to reflect the state of the game."""
         board = self.board.board
         for row_index, row in enumerate(board):
             for col_index in range(len(row)):
                 tile_widget = self.grid.itemAtPosition(row_index, col_index).widget()
                 tile_widget.setPixmap(QPixmap(App.number_mappings[board[row_index][col_index]]))
-                # tile_widget.setText(str(board[row_index][col_index]))
 
 
 if __name__ == '__main__':
